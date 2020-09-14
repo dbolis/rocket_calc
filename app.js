7537ad330adaffce911183d257729e3535421fc2
dbolis <danielbolis93@gmail.com>
2020-09-14 01:17:22 -0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 //Global values
-let F = 0// [N]
-let Isp = 0// [s]
-let mdot = 0// [kg/s]
-let cstar = 0// [m/s]
-let cf = 0// []
-let g0 = 9.81 // [m/s^2]
+// let F = 0// [N]
+// let Isp = 0// [s]
+// let mdot = 0// [kg/s]
+// let cstar = 0// [m/s]
+// let cf = 0// []
+// let g0 = 9.81 // [m/s^2]
+
+let vals = {
+  F: 0, // [N]
+  Isp: 0, // [s]
+  mdot: 0, // [kg/s]
+  cstar: 0, // [m/s]
+  cf: 0, // []
+  g0: 9.81 // [m/s^2]
+
+  }
+
 
 runEqsUp() //Load initial 0 values
-var decimals = 3 //Project wide decimals why only var works??
+
 
 /// Grab HTML ///
  
@@ -35,43 +46,40 @@ const cf_ThrustBox = document.getElementById("cf_ThrustBox")
 
 // mdot_Thrust
 document.getElementById("mdot_ThrustIn").addEventListener('input', function(e){
-  mdot_ThrustRange.value = e.target.value
-  // mdot = parseFloat(e.target.value)
-  let mdot = assignValue(e.target.value,[0,10])
-  // mdot_ThrustRange.value = mdot
-  mdot_ThrustIn.value = mdot
+  document.getElementById("mdot_ThrustRange").value = e.target.value
+  vals.mdot = assignValue(e.target.value,[5,100])
   runFuncs()
 })
 
 document.getElementById("mdot_ThrustRange").addEventListener('input', function(e){
-  mdot_ThrustIn.value = e.target.value
-  mdot = assignValue(e.target.value,[0,10])
+  document.getElementById("mdot_ThrustIn").value = e.target.value
+  vals.mdot = assignValue(e.target.value,[0,100])
   runFuncs()    
 })
 
 // cstar_Thrust 
 document.getElementById("cstar_ThrustIn").addEventListener('input', function(e){
-  cstar_ThrustRange.value = e.target.value
-  cstar = assignValue(e.target.value,[0,10])
+  document.getElementById("cstar_ThrustRange").value = e.target.value
+  vals.cstar = assignValue(e.target.value,[0,100])
   runFuncs()
 })
 
 document.getElementById("cstar_ThrustRange").addEventListener('input', function(e){
-  cstar_ThrustIn.value = e.target.value
-  cstar = assignValue(e.target.value,[0,10])   
+  document.getElementById("cstar_ThrustIn").value = e.target.value
+  vals.cstar = assignValue(e.target.value,[0,100])   
   runFuncs() 
 })
 
 // cf_Thrust 
 document.getElementById("cf_ThrustIn").addEventListener('input', function(e){
-  cf_ThrustRange.value = e.target.value
-  cf = assignValue(e.target.value,[0,10])
+  document.getElementById("cf_ThrustRange").value = e.target.value
+  vals.cf = assignValue(e.target.value,[0,100])
   runFuncs()
 })
 
 document.getElementById("cf_ThrustRange").addEventListener('input', function(e){
-  cf_ThrustIn.value = e.target.value
-  cf = assignValue(e.target.value,[0,10])
+  document.getElementById("cf_ThrustIn").value = e.target.value
+  vals.cf = assignValue(e.target.value,[0,100])
   runFuncs()  
 })
 
@@ -89,8 +97,8 @@ function runFuncs() {
 function runEqsUp() {
 
   //Level 1
-  F = Math.round((mdot*cf*cstar)*100)/100 //Thrust
-  Isp = Math.round((cf*cstar/g0)*100)/100 //Isp
+  vals.F = Math.round((vals.mdot*vals.cf*vals.cstar)*100)/100 //Thrust
+  vals.Isp = Math.round((vals.cf*vals.cstar/vals.g0)*100)/100 //Isp
 
 }
 
@@ -100,28 +108,48 @@ function updateRes() {
   const fRes = document.getElementById("fRes").firstChild
   const ispRes = document.getElementById("ispRes").firstChild
 
+  /// update result boxes ///
 
-  if (isNaN(F)) {
+  if (isNaN(vals.F)) {
     fRes.innerHTML=`Math Error`
   } else {
-    fRes.innerHTML = `F = ${F} [N]` 
+    fRes.innerHTML = `F = ${vals.F} [N]` 
   }
 
-  ispRes.innerHTML = `I<sub>sp</sub> = ${Isp} [s]`
+  ispRes.innerHTML = `I<sub>sp</sub> = ${vals.Isp} [s]`
+
+
+
+  /// update fields ///
+
+  //mdot
+  document.getElementById("mdot_ThrustRange").value = vals.mdot
+  document.getElementById("mdot_ThrustIn").value = vals.mdot
+
+  
 }  
 
 
 function assignValue(value, range) {
-  if (value ==""){
+  console.log(value)
+  if (value == ""){
     return ""
-  } else if(value=="-") {
-    return 0
-    // function warning to give message change field to 0
-  } else if(parseFloat(value)<range[0] || parseFloat(value)>range[1]) {
-    //function warning
+  } else if(parseFloat(value)>range[1]) {
+    //function warning too high 
+    errorMessage()
     return range[1]
+  } else if (parseFloat(value)<range[0]) {
+    // functino warning too low
+    return range[0]
   } else {
     return parseFloat(value)
   }
 
 }
+
+// function errorMessage() {
+//   setTimeout(function(){
+//     document.getElementById()
+//   })
+//   document.getElementById('badge').innerHTML="10 is too high"
+// }
